6292cf61e6b3c7252796904e08e4914732c25c80
limg <qazse41@sina.cn>
2017-04-06 20:01:38 +0800

    第一次上传git

---
+++ meadowlark.js
@@ -0,0 +1,44 @@
+const express = require('express')
+
+const app = express()
+
+//设置handlebars视图引擎
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.static(__dirname + '/public'));
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/about', (req, res) => {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
+})
+
+//定制404页面
+app.use((req, res) => {
+        res.status(404);
+        res.render('404')
+    })
+    //定制500页面
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500')
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.'
+    )
+})
